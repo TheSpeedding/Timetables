185392e093dd85eecdfbed128256fb18d0f31726
Lukas Riedel <thespeedding@gmail.com>
2018-02-27 09:28:26 +0100

    Error states added.

--- Timetables_Data_Preprocessor/GtfsParser/Downloader.cs
+++ Timetables_Data_Preprocessor/GtfsParser/Downloader.cs
@@ -6,39 +6,68 @@ namespace Timetables.Preprocessor
 {
     public static class Downloader
     {
-        public static void GetDataFeed(string path)
+        private readonly static Uri defaultUrl = new Uri(@"http://opendata.iprpraha.cz/DPP/JR/jrdata.zip");
+
+        [Flags]
+        public enum DownloadState { OK = 0, UrlInvalidFormat = 2, ResourceFileNotFound = 4, NoInternetConnection = 8, DefaultDataDownloaded = 16, UnknownError = 32  }
+        public static DownloadState GetDataFeed(string path)
         {
-            Uri url = null;
-            StreamReader sr = null;
+            DownloadState state = DownloadState.OK;
 
             try
             {
-                sr = new StreamReader("resource.txt");
-                url = new Uri(sr.ReadLine()); // Can throw Uri format exception.
+                Uri url = null;
+                StreamReader sr = null;
+
+                try
+                {
+                    sr = new StreamReader("resource.txt");
+                    url = new Uri(sr.ReadLine());
+                }
+                catch (FileNotFoundException)
+                {
+                    url = defaultUrl;
+                    state = DownloadState.ResourceFileNotFound | DownloadState.DefaultDataDownloaded;
+                }
+                catch (UriFormatException)
+                {
+                    url = defaultUrl;
+                    state = DownloadState.UrlInvalidFormat | DownloadState.DefaultDataDownloaded;
+                }
+                finally
+                {
+                    sr.Close();
+                    sr.Dispose();
+                }
+
+
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        client.DownloadFile(url, "data.zip");
+                    }
+                    catch (WebException)
+                    {
+                        state = DownloadState.NoInternetConnection;
+                    }
+                }
+
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+
+                Directory.CreateDirectory(path);
+
+                System.IO.Compression.ZipFile.ExtractToDirectory("data.zip", path);
+
+                File.Delete("data.zip");
             }
-            catch (FileNotFoundException)
+            catch (Exception)
             {
-                url = new Uri(@"http://opendata.iprpraha.cz/DPP/JR/jrdata.zip");
-            }       
-            catch (UriFormatException)
-            {
-                url = new Uri(@"http://opendata.iprpraha.cz/DPP/JR/jrdata.zip");
-            }
-            
-                
-            using (var client = new WebClient())
-            {
-                client.DownloadFile(url, "data.zip");
+                state = DownloadState.UnknownError;
             }
 
-            if (Directory.Exists(path))
-                Directory.Delete(path, true);
-
-            Directory.CreateDirectory(path);
-            
-            System.IO.Compression.ZipFile.ExtractToDirectory("data.zip", path);
-
-            File.Delete("data.zip");
+            return state;
         }
 
         public static void DeleteTrash(string path)
