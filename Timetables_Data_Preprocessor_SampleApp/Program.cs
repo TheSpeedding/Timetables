84e89c256abfe6e4079ceba9a750361063445d11
Lukas Riedel <thespeedding@gmail.com>
2018-03-19 10:32:53 +0100

    Preprocessor sample application improved.

--- Timetables_Data_Preprocessor_SampleApp/Program.cs
+++ Timetables_Data_Preprocessor_SampleApp/Program.cs
@@ -1,13 +1,27 @@
 ﻿using Timetables.Preprocessor;
+using System.Collections.Generic;
+using System;
 
 namespace Timetables.SampleApp
 {
 	class Program
 	{
-		// Prague only.
-		// static void Main(string[] args) => DataFeed.GetAndTransformDataFeed<GtfsDataFeed>("http://opendata.iprpraha.cz/DPP/JR/jrdata.zip");
+		static void Main(string[] args)
+		{
+			Console.WriteLine("You can enter as many URLs to data sources as you wish. Seperate them with a new line");
+			Console.WriteLine("Type END command to end the input.");
 
-		// Prague and Vienna data feeds merged.
-		static void Main(string[] args) => DataFeed.GetAndTransformDataFeed<GtfsDataFeed>("http://opendata.iprpraha.cz/DPP/JR/jrdata.zip", "https://go.gv.at/l9gtfs");
+			List<string> urls = new List<string> { "http://opendata.iprpraha.cz/DPP/JR/jrdata.zip" }; // Link to Prague data source added implicitly.
+
+			string line;
+			while ((line = Console.ReadLine()) != "END")
+				urls.Add(line);
+
+			Console.WriteLine($"{ DateTime.Now }: Starting data processing.");
+
+			DataFeed.GetAndTransformDataFeed<GtfsDataFeed>(urls.ToArray());
+
+			Console.WriteLine($"{ DateTime.Now }: Ending data processing.");
+		}
 	}
 }
