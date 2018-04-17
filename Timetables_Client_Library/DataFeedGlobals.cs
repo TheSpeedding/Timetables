aae6b96b4881f526cdc5948510ce7bff26fa3adb
Lukas Riedel <thespeedding@gmail.com>
2018-04-17 11:18:18 +0200

    Switched from WPF to WinForms.

--- Timetables_Client_Library/DataFeedGlobals.cs
+++ Timetables_Client_Library/DataFeedGlobals.cs
@@ -1,20 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Timetables.Preprocessor;
 
 namespace Timetables.Client
 {
+	/// <summary>
+	/// Class offering data feed for GUI applications.
+	/// </summary>
 	public static class DataFeedGlobals
 	{
 		private static Interop.DataFeedManaged fullData = null;
+		/// <summary>
+		/// Indicates whether application is working in offline mode (desktop application only).
+		/// </summary>
 		public static bool OfflineMode { get; } = true;
-		public static Structures.Basic.DataFeedBasic BasicData { get; }
-		public static Interop.DataFeedManaged FullData { get { return fullData ?? throw new NotSupportedException("The application is running in the offline mode. Cannot access data."); } }
+		/// <summary>
+		/// Basic data feed.
+		/// </summary>
+		public static Structures.Basic.DataFeedBasic Basic { get; private set; }
+		/// <summary>
+		/// Full data feed.
+		/// </summary>
+		public static Interop.DataFeedManaged Full { get { return fullData ?? throw new NotSupportedException("The application is running in the offline mode. Cannot access data."); } }
+		/// <summary>
+		/// Loads data while starting the application.
+		/// </summary>
 		static DataFeedGlobals()
 		{
-			BasicData = new Structures.Basic.DataFeedBasic();
+			// TO-DO: THIS IS ONLY TEMPORARY SOLUTION.
+			if (!System.IO.Directory.Exists("basic"))
+			{
+				DataFeed.GetAndTransformDataFeed<GtfsDataFeed>("http://opendata.iprpraha.cz/DPP/JR/jrdata.zip");
+			}
+
+			// TO-DO: Online mode?
+
+			Update();
+		}
+		/// <summary>
+		/// Updates data feed. Also used in static constructor.
+		/// </summary>
+		public static void Update()
+		{
+			Basic = new Structures.Basic.DataFeedBasic();
 
 			if (OfflineMode)
 			{
--- Timetables_Desktop_Application/Properties/Resources.Designer.cs
+++ Timetables_Desktop_Application/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Timetables.Desktop.Properties {
+namespace Timetables_Desktop_Application.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Timetables.Desktop.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Timetables.Desktop.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Timetables_Desktop_Application.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -63,9 +63,9 @@ namespace Timetables.Desktop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon icon {
+        internal static System.Drawing.Icon Icon {
             get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
