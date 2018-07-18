2bb15e19adea8bb350e425007788cd3244c6c2a4
Lukáš Riedel <thespeedding@gmail.com>
2018-07-18 22:12:09 +0200

    Server application project added, with some logging stuff.

---
+++ Timetables_Server_Application/Logging.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Timetables.Server
+{
+	/// <summary>
+	/// Class serving some methods to log server events.
+	/// </summary>
+	public static class Logging
+	{
+		/// <summary>
+		/// File that is the server logging into.
+		/// </summary>
+		public static StreamWriter FileForLogging { get; }
+		static Logging()
+		{
+			FileForLogging = new StreamWriter(".log", true);
+			FileForLogging.AutoFlush = true;
+		}
+
+		/// <summary>
+		/// Delegate used to log some information.
+		/// </summary>
+		/// <param name="message">Message of the log.</param>
+		public delegate void LoggingEventHandler(string message);
+		/// <summary>
+		/// Event that is fired to log some information.
+		/// </summary>
+		public static event LoggingEventHandler LoggingEvent;
+		/// <summary>
+		/// Adds current timestamp to the message.
+		/// </summary>
+		/// <param name="message">Message of the log.</param>
+		public static string LogWithDateTime(this string message) => DateTime.Now.ToString() + " - " + message;
+		/// <summary>
+		/// Fires the event for logging.
+		/// </summary>
+		/// <param name="message">Message to be logged.</param>
+		public static void Log(this string message) => LoggingEvent?.Invoke(message);
+	}
+}
---
+++ Timetables_Server_Application/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetables.Server
+{
+	class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			Logging.LoggingEvent += (string message) => Logging.FileForLogging.WriteLine(message.LogWithDateTime());
+			Logging.LoggingEvent += (string message) => Console.WriteLine(message.LogWithDateTime());
+
+			while (true)
+			{
+
+			}
+		}
+	}
+}
