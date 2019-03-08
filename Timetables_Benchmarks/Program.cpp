f25cd43c989129cd97c9726d4e65131b8ae4e03f
Lukas Riedel <thespeedding@gmail.com>
2019-03-08 21:56:04 +0100

    Benchmarks implemented.

--- Timetables_Benchmarks/Program.cpp
+++ Timetables_Benchmarks/Program.cpp
@@ -6,6 +6,11 @@
 #include <chrono>
 #include <tuple>
 
+#include <codecvt>
+#include <locale>
+
+#include <Windows.h>
+
 using namespace std;
 using namespace Timetables::Structures;
 using namespace Timetables::Algorithms;
@@ -27,30 +32,53 @@ static tuple<double, size_t, size_t, size_t> run_one_iteration(const data_feed& 
 	return make_tuple(duration, et_calls, marked_stops, routes_traversed);
 }
 
-static void run_benchmark(const data_feed& feed, size_t A, size_t B) {
-	constexpr size_t iterations = 100;
-
-	cout << "Running " + to_string(iterations) + " iterations with EDT at 2:00." << endl;	
-
+static void run_round(const data_feed& feed, size_t A, size_t B, const date_time& de, size_t iterations) {
 	double duration = 0;
 	size_t et_calls = 0;
 	size_t marked_stops = 0;
 	size_t routes_traversed = 0;
 
-	for (size_t i = 0; i < 100; ++i) {
-		auto res = run_one_iteration(feed, A, B, date_time(date_time::now().date(), HOUR * 2));
-		cout << "Time: " + to_string(get<0>(res)) + " ms. ET calls: " + to_string(get<1>(res)) + ". Marked stops: " + to_string(get<2>(res)) + ". Routes traversed: " + to_string(get<3>(res)) + "." << endl;
+	cout << endl << "Running " + to_string(iterations) + " iterations with EDT at " << de << "." << endl;
+
+	for (size_t i = 0; i < iterations; ++i) {
+		auto res = run_one_iteration(feed, A, B, de);
+
+		// cout << "Time: " + to_string(get<0>(res)) + " ms. ET calls: " + to_string(get<1>(res)) + ". Marked stops: " + to_string(get<2>(res)) + ". Routes traversed: " + to_string(get<3>(res)) + "." << endl;
+		
+		duration += get<0>(res);
+		et_calls += get<1>(res);
+		marked_stops += get<2>(res);
+		routes_traversed += get<3>(res);
 	}
 
-	cout << endl << "Averages:" << endl << "Time one journey: " << to_string(duration / (iterations * 50)) << " ms. Time: " + to_string(duration / iterations) + " ms. ET calls: " + to_string(et_calls / iterations) + ". Marked stops: " + to_string(marked_stops / iterations) + ". Routes traversed: " + to_string(routes_traversed / iterations) + ".";
+	cout << endl << "Averages (per 50 journeys):" << endl << " Time: " + to_string(duration / iterations) + " ms. ET calls: " + to_string(et_calls / iterations) + ". Marked stops: " + to_string(marked_stops / iterations) + ". Routes traversed: " + to_string(routes_traversed / iterations) + ".";
+
+}
+
+static void run_benchmark(const data_feed& feed, size_t A, size_t B) {
+	constexpr size_t iterations = 100;
+
+	wcout << endl << endl << L"Stations " << feed.stations().at(A).name() << L" and " << feed.stations().at(B).name() << L"." << endl;
+
+	run_round(feed, A, B, date_time(date_time::now().date(), HOUR * 2), 100);
+	run_round(feed, A, B, date_time(date_time::now().date(), HOUR * 8), 100);
+	run_round(feed, A, B, date_time(date_time::now().date(), HOUR * 13), 100);
 }
 
 int main() {
+	setlocale(LC_ALL, "");
+	SetConsoleCP(GetACP());
+	SetConsoleOutputCP(GetACP());
+
+	locale::global(locale("Czech"));
+
 	data_feed feed;
 
 	cout << "Data loaded successfully." << endl;
-	
+
 	run_benchmark(feed, feed.stations().find_index(L"Pod Jezerkou"), feed.stations().find_index(L"Malostranské námìstí"));
+	run_benchmark(feed, feed.stations().find_index(L"Horèièkova"), feed.stations().find_index(L"Letištì"));
+	run_benchmark(feed, feed.stations().find_index(L"Kladno,Americká"), feed.stations().find_index(L"Benešov,Pekárny"));
 
 	return 0;
 }
